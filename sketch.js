488940b4ce1cce88f5c6b4eab51fb68131d09e2e
not-mxgenta <emilyb9242@gmail.com>
2025-04-20 16:34:33 +0100

    fixed mouse coords debug and added font

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
-//dictating which 'stage' of the game we are in, changes the tilemaps and displayed assets
+//dictating which 'stage' of the game we are in, changes the background tilemaps and any events
 let currentGameState = 0;
+//more specific, works within each game state i.e. may be in state 2 (inside), dictates whether in location 0 (bathroom) or location 1 (bedroom) etc.
+let currentLocation = 1;
 
 //essential to centre all activity on the screen, regardless of screen size
 let newMouseX;
@@ -18,15 +20,35 @@ let BGtileSize = 256;
 //Image asset used for each tile (self-explanatory?)
 let tileImage;
 
-//Layout of different tile types for the background of the living room
+
+//Graphics maps for each environment, dictating placement of tiles for background
 //Oriented weirdly for some reason? Wasn't harming anyone so just left it lol
+
+//Layout of different tile types for the background of the living room
 let BGlivingRoomMap = [
-  [0, 1, 2],
-  [0, 1, 2],
-  [0, 1, 2],
-  [0, 1, 2],
-  [0, 1, 2]
+  [1, 2, 3],
+  [1, 2, 3],
+  [1, 2, 3],
+  [1, 2, 3],
+  [1, 2, 3]
 ]
+
+let BGkitchenMap = [
+  [1, 1, 1],
+  [1, 1, 1],
+  [1, 1, 1],
+  [1, 1, 1],
+  [1, 1, 1]
+]
+
+let BGbedroomMap = [
+  [1, 1, 1],
+  [1, 1, 1],
+  [1, 1, 1],
+  [1, 1, 1],
+  [1, 1, 1]
+]
+
 
 //Used for background tiles of any given scene
 class backgroundTile {
@@ -53,10 +75,18 @@ function preload() {
   //overlay video
   VHSoverlay = createVideo("assets/vhsOverlay.mp4")
 
+  //font
+  VT323Font = loadFont("assets/fonts/VT323-Regular.ttf")
+
   //background tiles
+  //living room
   BGwallpaper = loadImage("assets/BG_Wallpaper.png")
   BGpanellingLower = loadImage("assets/BG_PanellingLower.png")
   BGpanellingUpper = loadImage("assets/BG_PanellingUpper.png")
+  //kitchen
+  BGkitchen = loadImage("assets/BG_KitchenTiles.png")
+  //bedroom
+  BGbedroom = loadImage("assets/BG_BedroomWall.png")
 
 }
 
@@ -94,7 +124,7 @@ function setup() {
   VHSoverlay.volume(0);
   VHSoverlay.play()
 
-  BGtiles()
+  BGtilesInside()
 
 }
 
@@ -110,7 +140,8 @@ function mouseClicked() {
 }
 
 
-function BGtiles() {
+function BGtilesInside() {
+
   let tileID = 0
   
   //resets existing tile map to clean/empty map for new environment to be added
@@ -128,13 +159,19 @@ function BGtiles() {
     for (let tileY = 0; tileY < BGtilesY; tileY++) {
 
       //changes the image asset to use for the tile based on its value in the graphics map declared at the beginning
-      //each environment uses a different graphics map, dictated by the current game state
-      if (BGlivingRoomMap[tileX][tileY] == 0) {
-        tileImage = BGwallpaper
-      } else if (BGlivingRoomMap[tileX][tileY] == 1) {
-        tileImage = BGpanellingUpper
-      } else if (BGlivingRoomMap[tileX][tileY] == 2) {
-        tileImage = BGpanellingLower
+      //each environment uses a different graphics map, dictated by the current game state and location
+      if (currentLocation == 1) {
+        if (BGlivingRoomMap[tileX][tileY] == 1) {
+          tileImage = BGwallpaper
+        } else if (BGlivingRoomMap[tileX][tileY] == 2) {
+          tileImage = BGpanellingUpper
+        } else if (BGlivingRoomMap[tileX][tileY] == 3) {
+          tileImage = BGpanellingLower
+        }
+      } else if (currentLocation == 2) {
+        if (BGkitchenMap[tileX][tileY] == 1) {
+          tileImage = BGbedroom
+        }
       }
 
       //adds new tile to tile map!
@@ -147,7 +184,8 @@ function BGtiles() {
 
 function draw() {
   background('black')
-  currentGameState = 2 //inside (testing only)
+  currentGameState = 0
+  currentLocation = 1
 
   //Calculates new mouse coordinates based on center of screen instead of default top left corner, thus allowing coordinates to remain same regardless of window resizing - crucial when calculating mouse click position across different window sizes
   newMouseX = mouseX - (windowWidth/2)
@@ -176,14 +214,15 @@ function draw() {
   //reset translations (so they don't just accumulate with every run of the draw function)
   pop()
 
-  //not currently working :(
-  fill('white')
-  text(newMouseX, mouseX+50, mouseY)
-  text(newMouseY, mouseX+50, mouseY + 10)
-
-
   //semi-transparent VHS-style overlay
   tint(255, 150);
   image(VHSoverlay, 0, 0, 1280, 768);
+
+  //track mouse coordinates (useful for tracking click position later)
+  fill('white')
+  textFont(VT323Font, 30)
+  textAlign(CENTER, CENTER)
+  text(newMouseX, newMouseX+50, newMouseY)
+  text(newMouseY, newMouseX+50, newMouseY + 30)
   
 }
